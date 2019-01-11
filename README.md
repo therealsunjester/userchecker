3a5a56f378ce160fd90cba5e17ea54153e637309
therealsunjester <sunjester@protonmail.com>
2019-01-11 06:55:28 -0700

    fitst

---
+++ README.md
@@ -0,0 +1,2 @@
+## Username Checker for Social Media
+This will check for a valid or invalid username to a list of social media websites, with no dependencies.
---
+++ social.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#sunjester
+echo "checking for username: "$@
+
+RED=`tput setaf 1`
+GRN=`tput setaf 2`
+RST=`tput sgr0`
+
+sites=(
+	"https://www.facebook.com"
+	"https://www.instagram.com"
+	"https://www.twitter.com"
+	"https://www.youtube.com/user"
+	"https://www.reddit.com/user"
+)
+
+for site in "${sites[@]}"
+do
+	res=$(curl -Is $site"/"$@"/" >social;head -n1 social)
+	code=`echo $res|cut -d' ' -f2`
+	case $code in
+		200)
+		echo $GRN $site"/"$@"/" $RST
+		;;
+		404)
+		echo $RED $site"/"$@"/" $RST
+		;;
+                301)
+                echo $GRN $site"/"$@"/" $RST
+                ;;
+                405)
+                echo $RED $site"/"$@"/" $RST
+                ;;
+	esac
+done
+
+rm social
